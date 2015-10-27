a16ed5940e7f168ed6b7ff449e974ca202735ff7
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2015-10-27 18:00:26 +0100

    fiddling with listview ...

--- app/src/main/groovy/app/georchestra/beneth/fr/georchestra/MainActivity.groovy
+++ app/src/main/groovy/app/georchestra/beneth/fr/georchestra/MainActivity.groovy
@@ -2,6 +2,7 @@ package app.georchestra.beneth.fr.georchestra
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.TextView
 
@@ -13,10 +14,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ListView sv = (ListView) this.findViewById(R.id.wxsServersListView)
+
+        String[] arr = new String[10];
+
         for (int i = 0 ; i < 10 ; ++i) {
-            TextView tv = new TextView(this)
-            tv.setText("Current elem " + i)
-            sv.addFooterView(tv)
+           arr[i] = "Current elem " + i;
         }
+        def aa = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arr)
+        sv.setAdapter(aa)
     }
 }

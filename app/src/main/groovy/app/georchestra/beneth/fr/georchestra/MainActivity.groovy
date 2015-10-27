8db1f083e9f424430fba6d87044bf03c25d110d6
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2015-10-27 17:45:07 +0100

    Some tries with Android widgets

--- app/src/main/groovy/app/georchestra/beneth/fr/georchestra/MainActivity.groovy
+++ app/src/main/groovy/app/georchestra/beneth/fr/georchestra/MainActivity.groovy
@@ -1,7 +1,9 @@
-package app.georchestra.beneth.fr.georchestra;
+package app.georchestra.beneth.fr.georchestra
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ListView
+import android.widget.TextView
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView sv = (ListView) this.findViewById(R.id.wxsServersListView)
+        for (int i = 0 ; i < 10 ; ++i) {
+            TextView tv = new TextView(this)
+            tv.setText("Current elem " + i)
+            sv.addFooterView(tv)
+        }
     }
 }

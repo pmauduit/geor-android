2f4c10cb7ffe9d5e1a5a3ae4e9adb24a60d320cd
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2015-10-28 16:34:10 +0100

    Adding the url to the SDI

--- app/src/main/groovy/app/georchestra/beneth/fr/georchestra/RetrieveGeorInstancesTask.groovy
+++ app/src/main/groovy/app/georchestra/beneth/fr/georchestra/RetrieveGeorInstancesTask.groovy
@@ -2,10 +2,12 @@ package app.georchestra.beneth.fr.georchestra
 
 import android.app.ProgressDialog
 import android.content.Context;
-import android.os.AsyncTask;
+import android.os.AsyncTask
+import android.view.View
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
+import android.widget.ListView
+import android.widget.TextView;
 import fr.beneth.wxslib.georchestra.Instance;
 
 /**
@@ -30,9 +32,19 @@ class RetrieveGeorInstancesTask extends AsyncTask<Object, Void, Object> {
     @Override
     protected void onPostExecute(Object result) {
         ListView lv = (ListView) activity.findViewById(R.id.wxsServersListView)
-        def arr = geOrInstances.findAll { it.title != "" }.collect { it.title }
-        def aa = new ArrayAdapter(activity, android.R.layout.simple_list_item_1,
-                arr.toArray(new String[arr.size()]))
+        def arr = geOrInstances.findAll { it.title != ""  && it.isPublic }
+
+        def aa = new ArrayAdapter(activity, android.R.layout.simple_list_item_2, android.R.id.text1, arr) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView text1 = (TextView) view.findViewById(android.R.id.text1);
+                TextView text2 = (TextView) view.findViewById(android.R.id.text2);
+                text1.setText(arr.get(position).title);
+                text2.setText(arr.get(position).url);
+                return view;
+            }
+        }
         lv.setAdapter(aa)
         return
     }
